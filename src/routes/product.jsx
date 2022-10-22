cc78bbdd3e21a434f4cc9aaa75c6c5c5f3cd6c85
Bakhtyiar Klimov <baxtiar00@gmail.com>
2022-10-22 13:10:04 +0300

    added container with row and cols

--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Badge, Breadcrumb, Carousel, Spinner } from 'react-bootstrap';
+import { Badge, Breadcrumb, Carousel, Col, Container, Row, Spinner } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { routes } from '../utils/constants';
 import { getProduct } from '../utils/methods';
+
+const StyledContainer = styled(Container)`
+	padding-top: 16px;
+`;
 
 const StyledCarousel = styled(Carousel)`
 	width: fit-content;
@@ -23,7 +27,7 @@ const Product = () => {
 	}, [id]);
 
 	return (
-		<>
+		<StyledContainer>
 			<Breadcrumb>
 				<Breadcrumb.Item ><Link to={routes.home.path}>Home</Link></Breadcrumb.Item>
 				<Breadcrumb.Item><Link to={routes.products.path}>Products</Link></Breadcrumb.Item>
@@ -37,25 +41,29 @@ const Product = () => {
 						</Spinner>
 					</>
 				) : (
-					<div>
-						<h1>{data.title}</h1>
-						<h2>{data.description}</h2>
-						<h3>Price: ${data.price} Discount:  <Badge bg="warning" text="dark">{data.discountPercentage}%</Badge></h3>
-						<p>Rating: {data.rating}</p>
-						<p>Stock: {data.stock}</p>
-						<p>Brand: {data.brand}</p>
-						<p>Category: {data.category}</p>
-						<StyledCarousel>
-							{data.images.map((item, index) => (
-								<Carousel.Item>
-									<img key={item} src={item} alt={`Carousel item ${index} slide`} />
-								</Carousel.Item>
-							))}
-						</StyledCarousel>
-					</div>
+					<Row>
+						<Col>
+							<h1>{data.title}</h1>
+							<h2>{data.description}</h2>
+							<h3>Price: ${data.price} Discount:  <Badge bg="warning" text="dark">{data.discountPercentage}%</Badge></h3>
+							<p>Rating: {data.rating}</p>
+							<p>Stock: {data.stock}</p>
+							<p>Brand: {data.brand}</p>
+							<p>Category: {data.category}</p>
+						</Col>
+						<Col>
+							<StyledCarousel>
+								{data.images.map((item, index) => (
+									<Carousel.Item>
+										<img key={item} src={item} alt={`Carousel item ${index} slide`} />
+									</Carousel.Item>
+								))}
+							</StyledCarousel>
+						</Col>
+					</Row>
 				)
 			}
-		</>
+		</StyledContainer>
 	);
 }
 
