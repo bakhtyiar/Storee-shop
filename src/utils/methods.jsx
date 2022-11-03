6832dcfa338faa34d4cab8f6572e77ce91b1377b
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-11-03 19:38:39 +0300

    methods.jsx style editing of code

--- src/utils/methods.jsx
+++ src/utils/methods.jsx
@@ -3,18 +3,11 @@ import {dummyjsonURL, pageLimit} from './constants';
 export const getData = async (skip = 0, limit = pageLimit, url = dummyjsonURL) => {
 	return (fetch(`${url}/products?skip=${skip}&limit=${limit}`) 
 	.catch(e => console.log(e))
-	.then(response => response.json())
-	.then(data => {
-		return data;
-	}))
+	.then(response => response.json()))
 };
 
 export const getProduct = async (id, url = dummyjsonURL) => {
 	return (fetch(`${url}/products/${id}`) 
 	.catch(e => console.log(e))
-	.then(response => response.json())
-	.then(data => {
-		console.log(data);
-		return data;
-	}))
+	.then(response => response.json()))
 };
