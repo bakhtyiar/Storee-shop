381e9a6be42f1dd5ffbfa514fb7fe452abef50c6
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-11-29 19:31:02 +0300

    edited home.jsx added categories to page

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,39 +1,70 @@
-import {Button, InputGroup, Form, Container} from 'react-bootstrap';
-import React, { useState } from 'react'
-
+import {Button, InputGroup, Form, Container, Row} from 'react-bootstrap';
+import React, {useEffect, useState} from 'react'
+import CategoryCard from "../components/categoryCard";
+import {getCategories} from "../utils/methods";
 // todo add panels with few o news, o products, o users
 
 export default function Home() {
-	const [searchValue, setSearchValue] = useState("");
-	const [searchHints, setSearchHints] = useState(["harry potter", "chess", "physics", "tolstoy"]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [categories, setCategories] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const [searchHints, setSearchHints] = useState(["harry potter", "chess", "physics", "tolstoy"]);
 
-	const handleFind = () => {
-	};
 
-	return (
-		<Container>
-			<h1>Home</h1>
-				<InputGroup className="mb-3">
-					<Form.Control
-						placeholder="Recipient's username"
-						id="searchInput"
-						list="searchHints"
-						value={searchValue}
-						onChange={e => setSearchValue(e.target.value)}
-						aria-label="Recipient's username"
-						aria-describedby="basic-addon2"
-					/>
-					<datalist id="searchHints">
-						{
-							searchHints.map((value => (
-								<option value={value} key={value} />
-							)))
-						}
-					</datalist>
-					<Button variant="outline-secondary" id="button-addon2" onClick={handleFind}>
-						Find
-					</Button>
-				</InputGroup>
-		</Container>
-	)
+    const handleFind = () => {
+    };
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/quotes/random')
+            .then(res => res.json())
+            .then(console.log);
+    }, []);
+
+    useEffect(() => {
+        setIsLoading(true);
+        getCategories()
+            .then(data => setCategories(data))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+
+    return (
+        <Container>
+            <Row>
+                <h1>Home</h1>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        placeholder="Recipient's username"
+                        id="searchInput"
+                        list="searchHints"
+                        value={searchValue}
+                        onChange={e => setSearchValue(e.target.value)}
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <datalist id="searchHints">
+                        {
+                            searchHints.map((value => (
+                                <option value={value} key={value}/>
+                            )))
+                        }
+                    </datalist>
+                    <Button variant="outline-secondary" id="button-addon2" onClick={handleFind}>
+                        Find
+                    </Button>
+                </InputGroup>
+            </Row>
+            <Row>
+                {!isLoading &&
+                    <div style={{'display': 'flex', 'gap': '18px', 'flex-wrap': 'wrap'}}>
+                        {
+                            categories.map((item) => (
+                                <CategoryCard header={item}/>
+                            ))
+                        }
+                    </div>
+                }
+            </Row>
+        </Container>
+    )
 }
