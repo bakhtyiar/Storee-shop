8bb9882cd30d7f160964766cba988cace05efecc
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-11-29 19:25:34 +0300

    added categoryCard.jsx

---
+++ src/components/categoryCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {routes} from "../utils/constants";
+import {Link} from "react-router-dom";
+import {Card} from "react-bootstrap";
+import {capitalizeStr, replaceDashToSpace} from "../utils/methods";
+
+const CategoryCard = (props) => {
+    let header = props.header;
+    header = capitalizeStr(header);
+    header = replaceDashToSpace(header);
+
+    return (
+        <Link to={routes.products.path} style={{'color': 'black', 'text-decoration': 'none'}}>
+            <Card
+                bg={'Light'.toLowerCase()}
+                style={{width: '18rem'}}
+                className="mb-2"
+            >
+                <Card.Body>
+                    <Card.Title style={{'margin': '0'}}>{header}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Link>
+    );
+};
+
+export default CategoryCard;
