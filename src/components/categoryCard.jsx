e98b6699957afe32ca5c9c5fd98492dbdda9cbe2
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-01 18:38:03 +0300

    edited categoryCard.jsx added link to categories

--- src/components/categoryCard.jsx
+++ src/components/categoryCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import {routes} from "../utils/constants";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {Card} from "react-bootstrap";
 import {capitalizeStr, replaceDashToSpace} from "../utils/methods";
 
 const CategoryCard = (props) => {
     let header = props.header;
+    let navigate = useNavigate();
     header = capitalizeStr(header);
     header = replaceDashToSpace(header);
 
     return (
-        <Link to={routes.products.path} style={{'color': 'black', 'textDecoration': 'none'}}>
+        <div onClick={() => navigate(`${routes.products.path}/1?category=${props.header}`)}
+             style={{'color': 'black', 'textDecoration': 'none'}}>
             <Card
                 bg={'Light'.toLowerCase()}
                 style={{width: '18rem'}}
@@ -20,7 +22,7 @@ const CategoryCard = (props) => {
                     <Card.Title style={{'margin': '0'}}>{header}</Card.Title>
                 </Card.Body>
             </Card>
-        </Link>
+        </div>
     );
 };
 
