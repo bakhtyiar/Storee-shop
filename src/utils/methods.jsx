c7b8eb113f36fd414f0c3ac6619222653a36f2e7
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-03 01:34:54 +0300

    restyled methods.jsx tabs, added loginUser and registerUser method

--- src/utils/methods.jsx
+++ src/utils/methods.jsx
@@ -1,43 +1,73 @@
 import {dummyjsonURL, pageLimit} from './constants';
 
 export const getProducts = async (skip = 0, limit = pageLimit, category = 'tops', url = dummyjsonURL) => {
-	if (category !== '') {
-		category = `/category/${category}`;
-	}
-	return (fetch(`${url}/products${category}?skip=${skip}&limit=${limit}`)
-	.catch(e => console.log(e))
-	.then(response => response.json()))
+    if (category !== '') {
+        category = `/category/${category}`;
+    }
+    return (fetch(`${url}/products${category}?skip=${skip}&limit=${limit}`)
+        .catch(e => console.log(e))
+        .then(response => response.json()))
 };
 
 export const getProduct = async (id, url = dummyjsonURL) => {
-	return (fetch(`${url}/products/${id}`) 
-	.catch(e => console.log(e))
-	.then(response => response.json()))
+    return (fetch(`${url}/products/${id}`)
+        .catch(e => console.log(e))
+        .then(response => response.json()))
 };
 
 export const getCategories = async (url = dummyjsonURL) => {
-	return (fetch(`${url}/products/categories`)
-		.catch(e => console.log(e))
-		.then(response => response.json()))
+    return (fetch(`${url}/products/categories`)
+        .catch(e => console.log(e))
+        .then(response => response.json()))
 }
 
 export const getUsers = async (query, url = dummyjsonURL) => {
-	return (fetch(`${url}/users/search?q=${query}`)
-		.catch(e => console.log(e))
-		.then(response => response.json()))
+    return (fetch(`${url}/users/search?q=${query}`)
+        .catch(e => console.log(e))
+        .then(response => response.json()))
 }
 
 export const getUser = async (id, url = dummyjsonURL) => {
-	return (fetch(`${url}/users/search?q=${id}`)
-		.catch(e => console.log(e))
-		.then(response => response.json())
-		.then(data => data["users"][0]))
+    return (fetch(`${url}/users/${id}`)
+        .catch(e => console.log(e))
+        .then(response => response.json()))
+}
+
+export const registerUser = async (username, email, password, url = dummyjsonURL) => {
+    return (fetch(`${url}/users/add`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'username': username,
+                'email': email,
+                'password': password,
+                /* other user data */
+            })
+        })
+            .then(res => res.json())
+    )
+}
+
+export const loginUser = async (username, password, url = dummyjsonURL) => {
+    return (
+        fetch(`${url}/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+
+                'username': username,
+                'password': password,
+                // expiresInMins: 60, // optional
+            })
+        })
+            .then(res => res.json())
+    )
 }
 
 export const capitalizeStr = (str) => {
-	return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export const replaceDashToSpace = (str) => {
-	return str.replace(/-+/g, ' ');
+    return str.replace(/-+/g, ' ');
 }
