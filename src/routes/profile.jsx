98d61bffa998ff5063aa5d1c02a004a50a3965c7
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-03 16:17:15 +0300

    edited profile.jsx

--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -1,14 +1,12 @@
 import React, {Fragment, useContext, useEffect, useState} from 'react';
-import {Container} from "react-bootstrap";
-import {getUser} from "../utils/methods";
+import {Button, Container, Image} from "react-bootstrap";
 import {RootContext} from "../contexts/root-context";
-import {useNavigate, useParams} from "react-router-dom";
-import {routes} from "../utils/constants";
+import avatarPlaceholder from "../assets/img/personalisation/avatar-placeholder.png";
 
 //todo: add editiing of personal data, add 'quit' button, add profile image and edit function for it
 
 const Profile = () => {
-    const { authUserState: { username, image, email, password } } = useContext(RootContext);
+    const { authUserState: { username, image, email, password, onLogout} } = useContext(RootContext);
     const [isLoading, setIsLoading] = useState(false);
 
     // The original idea. Not realized due to api restrictions
@@ -27,15 +25,31 @@ const Profile = () => {
     //         .finally(() => setIsLoading(false));
     // }, []);
 
+    let avatar = avatarPlaceholder;
+    useEffect(() => {
+        avatar = image == '' ? avatarPlaceholder : image;
+    }, [image, avatarPlaceholder]);
+
+
     return (
         <Container>
             <h4>Profile</h4>
             {isLoading ? "Data is loading..." : (
                 <Fragment>
+                    <Image
+                        src={avatar}
+                        alt="Profile's avatar"
+                        roundedCircle
+                        style={{
+                            width: '128px',
+                            height: '128px',
+                        }}
+                    />
                     <h2>{username}</h2>
                     <p>email: {email}</p>
                     <p>username: {username}</p>
                     <p>password: ***</p>
+                    <Button>Logout</Button>
                 </Fragment>
             )}
         </Container>
