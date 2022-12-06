a4546eeb2b97e5b1fef86d6e083972304ec286fd
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-07 01:07:22 +0300

    removed exceeded tags, styles

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,38 +4,35 @@ import imageIcon from '../../assets/img/icons/image/icons8-image-48.png';
 import {routes} from '../../utils/constants';
 import {Link} from 'react-router-dom';
 
-const StyledProductCard = styled.li`
-	padding: 12px 8px;
-	border-radius: 8px;
-	border: 1px solid #cecece;
-	display: flex;
-	align-items: center;
-	& * {
-		margin: 0;
-	}
-`;
+const StyledProductCard = styled(Link)`
+  padding: 12px 8px;
+  border-radius: 8px;
+  border: 1px solid #cecece;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: inherit;
+  text-decoration: inherit;
 
-const StyledLink = styled(Link)`
-	color: inherit;
-	text-decoration: inherit;
+  & * {
+    margin: 0;
+  }
 `;
 
 const StyledImg = styled.img`
-	align-self: center;
-	max-width: 100%;
-	max-height: 200px;
+  align-self: center;
+  max-width: 100%;
+  max-height: 200px;
 `;
 
 const ProductCard = ({item}) => {
-	return (
-		<StyledLink to={`${routes.product.path}/${item.id}`}>
-			<StyledProductCard key={item.id}>
-				<StyledImg src={imageIcon} alt="image icon" />
-				<h6>{item.title}</h6>
-				<p>Price: ${item.price}</p>
-			</StyledProductCard>
-		</StyledLink>
-	);
+    return (
+        <StyledProductCard to={`${routes.product.path}/${item.id}`} key={item.id}>
+            <StyledImg src={imageIcon} alt="image icon"/>
+            <h6>{item.title}</h6>
+            <p>Price: ${item.price}</p>
+        </StyledProductCard>
+    );
 }
 
 export default ProductCard;
