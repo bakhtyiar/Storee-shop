a947ea1ef980013b274e7b7d4b0e27bc98b44155
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-09 17:14:23 +0300

    added handler to button product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components';
 import imageIcon from '../../assets/img/icons/image/icons8-image-48.png';
 import {routes} from '../../utils/constants';
 import {Link} from 'react-router-dom';
 import {Button} from "react-bootstrap";
+import {RootContext} from "../../contexts/root-context";
 
 const StyledImg = styled.img`
   align-self: center;
@@ -12,12 +13,21 @@ const StyledImg = styled.img`
 `;
 
 const ProductCard = ({item}) => {
+    const { cartState: { onAddToCart } } = useContext(RootContext);
+
     return (
         <Link className='px-2 py-3 border rounded d-flex flex-column align-items-start text-decoration-none text-black' to={`${routes.product.path}/${item.id}`} key={item.id}>
             <StyledImg src={imageIcon} alt="image icon"/>
             <h6 className='mt-2'>{item.title}</h6>
             <p>Price: ${item.price}</p>
-            <Button variant='dark' className='w-100'>
+            <Button
+                variant='dark'
+                className='w-100'
+                onClick={ (e) => {
+                    e.preventDefault();
+                    onAddToCart(item.id);
+                }}
+            >
                 <i className="bi bi-bag-plus"></i>
                 {' '}
                 Add to cart
