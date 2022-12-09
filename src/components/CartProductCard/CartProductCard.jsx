5e9539117b4a66f59fec2ee5e942ce40a8a96a9d
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-09 19:39:59 +0300

    edit style CartProductCard.jsx

--- src/components/CartProductCard/CartProductCard.jsx
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -17,13 +17,16 @@ const CartProductCard = ({product, selfIndexInCart}) => {
 
     return (
         <Link className='px-2 py-3 border rounded d-flex flex-row align-items-center text-decoration-none text-black' to={`${routes.product.path}/${product.id}`}>
-            <StyledImg src={imageIcon} alt="image icon"/>
-            <h6 className='mt-2'>{product.title}</h6>
-            <p className='my-0'>Price: {product.price}</p>
-            <p>Amount: {product.quantity}</p>
+            <div className='w-100 d-flex flex-row'>
+                <StyledImg src={imageIcon} alt="image icon"/>
+                <div className='flex-column ms-2'>
+                    <h6 className='mt-2'>{product.title}</h6>
+                    <p className='my-0'>Price: {product.price}</p>
+                </div>
+            </div>
             <Button
-                variant='dark'
-                className='w-100'
+                variant='outline-dark'
+                className='d-flex rounded'
                 onClick={ (e) => {
                     e.preventDefault();
                     onRemoveFromCart(selfIndexInCart);
@@ -31,7 +34,7 @@ const CartProductCard = ({product, selfIndexInCart}) => {
             >
                 <i className="bi bi-x"></i>
                 {' '}
-                Remove from cart
+                Remove
             </Button>
         </Link>
     );
