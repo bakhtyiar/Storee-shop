8406601bf75b5b486fc03d29a91765903551de3c
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-09 17:09:50 +0300

    added cart page

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import {RootContext} from "../contexts/root-context";
+import CartProductCard from "../components/CartProductCard/CartProductCard";
 
 const Cart = () => {
+    const {cartState: {products, total, discountedTotal, totalQuantity}} = useContext(RootContext);
     return (
-        <div>
-            <h1>Cart</h1>
-        </div>
+        <Container>
+            <Row >
+                <Col xs={12} md={8}>
+                    <h1>Cart</h1>
+                    {products.length > 0 ? (
+                        <>
+                        {products.map((product, index) => (
+                            <CartProductCard product={product} selfIndexInCart={index}/>
+                        ))}
+                        <h5>Amount: {totalQuantity}</h5>
+                        </>
+                    ) : (
+                        <h3>Nothing here</h3>
+                    )}
+                </Col>
+                <Col xs={12} md={4}>
+                    <h3>R Col</h3>
+                    <p>Total: </p>
+                    <p className='text-decoration-line-through'>{total}</p>
+                    <p>{discountedTotal}</p>
+                    <p></p>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
