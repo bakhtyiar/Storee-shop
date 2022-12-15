8b7a47244f89bd9455ddd58279655f36c13a4974
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-15 19:27:05 +0300

    cart page layout edits

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -4,9 +4,9 @@ import {RootContext} from "../contexts/root-context/root-context";
 import CartProductCard from "../components/CartProductCard/CartProductCard";
 
 const Cart = () => {
-    const {cartState: {products, total, totalQuantity}} = useContext(RootContext);
+    const {cartState: {products, total, discountedTotal, totalProducts, totalQuantity}} = useContext(RootContext);
     return (
-        <Container>
+        <Container className='mt-4'>
             <Row>
                 <Col xs={12} md={8}>
                     <h1>Cart</h1>
@@ -15,7 +15,7 @@ const Cart = () => {
                             {products.map((product, index) => (
                                 <CartProductCard product={product} selfIndexInCart={index} key={product.id}/>
                             ))}
-                            <h5 className='mt-3'>Amount: {totalQuantity}</h5>
+                            <h5 className='mt-3'>Amount: {totalProducts} Quantity: {totalQuantity}</h5>
                         </>
                     ) : (
                         <h3>Nothing here</h3>
@@ -23,7 +23,7 @@ const Cart = () => {
                 </Col>
                 <Col xs={12} md={4}>
                     <h3>R Col</h3>
-                    <p>Total: {total}</p>
+                    <p>Total: <b style={{textDecoration: 'line-through'}}>{total}</b> {discountedTotal}</p>
                 </Col>
             </Row>
         </Container>
