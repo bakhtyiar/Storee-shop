af43d0854ca545db3ef0f62012f64983b3f4ca2b
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-16 13:28:24 +0300

    added quantity controls

--- src/components/CartProductCard/CartProductCard.jsx
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {RootContext} from "../../contexts/root-context/root-context";
 import {Link} from "react-router-dom";
 import {routes} from "../../utils/constants";
 import imageIcon from "../../assets/img/icons/image/icons8-image-48.png";
-import {Button} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import styled from "styled-components";
 
 const StyledImg = styled.img`
@@ -13,30 +13,68 @@ const StyledImg = styled.img`
 `;
 //todo : add amount of per product, add controls to make a buy, add list in right column of discounts
 const CartProductCard = ({product, selfIndexInCart}) => {
-    const { cartState: { onRemoveFromCart } } = useContext(RootContext);
+    const {cartState: {onRemoveFromCart, onUpdateQuantity}} = useContext(RootContext);
+    const [quantity, setQuantity] = useState(product.quantity);
+
+    useEffect(() => {
+        if (quantity < 1) {
+            onRemoveFromCart(selfIndexInCart);
+        } else {
+            onUpdateQuantity(product.id, quantity);
+        }
+    }, [quantity]);
 
     return (
-        <Link className='px-2 py-3 border rounded d-flex flex-row align-items-center text-decoration-none text-black' to={`${routes.product.path}/${product.id}`}>
-            <div className='w-100 d-flex flex-row'>
+        <div className='px-2 py-3 border rounded d-flex flex-row align-items-center text-decoration-none text-black'>
+            <Link className='w-100 d-flex flex-row text-decoration-none text-black'
+                  to={`${routes.product.path}/${product.id}`}>
                 <StyledImg src={imageIcon} alt="image icon"/>
                 <div className='flex-column ms-2'>
                     <h6 className='mt-2'>{product.title}</h6>
                     <p className='my-0'>Price: {product.price}</p>
                 </div>
+            </Link>
+            <div className='ml-3 d-flex gap-2'>
+                <div className='d-flex'>
+                    <Button
+                        variant="outline-dark"
+                        onClick={() => {
+                            setQuantity((prevState) => prevState - 1);
+                        }}
+                    >
+                        -
+                    </Button>
+                    <Form.Control
+                        type="number"
+                        value={quantity}
+                        placeholder="Quantity"
+                        onChange={(e) => {
+                            setQuantity(Number(e.target.value));
+                        }}
+                    />
+                    <Button
+                        variant="outline-dark"
+                        onClick={() => {
+                            setQuantity((prevState) => prevState + 1);
+                        }}
+                    >
+                        +
+                    </Button>
+                </div>
+                <Button
+                    variant='outline-dark'
+                    className='d-flex rounded'
+                    onClick={(e) => {
+                        e.preventDefault();
+                        onRemoveFromCart(selfIndexInCart);
+                    }}
+                >
+                    <i className="bi bi-x"></i>
+                    {' '}
+                    Remove
+                </Button>
             </div>
-            <Button
-                variant='outline-dark'
-                className='d-flex rounded'
-                onClick={ (e) => {
-                    e.preventDefault();
-                    onRemoveFromCart(selfIndexInCart);
-                }}
-            >
-                <i className="bi bi-x"></i>
-                {' '}
-                Remove
-            </Button>
-        </Link>
+        </div>
     );
 };
 
