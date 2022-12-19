bf848ce93708a89322715ebb416b2465fa44c798
Bakhtiyar Khuramshin <bakhtiyarkhuramshin@gmail.com>
2022-12-19 21:02:33 +0300

    added authHeader method

---
+++ src/utils/server-api/server-api.js
@@ -0,0 +1,12 @@
+export default function authHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.token) {
+        return {
+            Authorization: 'Bearer ' + user.token,
+            'Content-Type': 'application/json',
+        };
+    } else {
+        return {'Content-Type': 'application/json',};
+    }
+}
