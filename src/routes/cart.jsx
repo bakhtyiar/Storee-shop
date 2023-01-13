479dca42814cae591e71f9ae7a85b2481882903f
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-01-13 16:12:18 +0300

    layout fixes

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,24 +1,25 @@
 import React, {useContext} from 'react';
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {RootContext} from "../contexts/root-context/root-context";
 import CartProductCard from "../components/CartProductCard/CartProductCard";
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {routes} from "../utils/constants";
 
 const Cart = () => {
     const {cartState: {products, total, discountedTotal, totalProducts, totalQuantity}} = useContext(RootContext);
-    const navigate = useNavigate();
 
     return (
         <Container className='mt-4'>
             <Row>
+                <h2 className='mb-3'>Cart</h2>
                 <Col xs={12} md={8}>
-                    <h1>Cart</h1>
                     {products.length > 0 ? (
                         <>
-                            {products.map((product, index) => (
-                                <CartProductCard product={product} selfIndexInCart={index} key={product.id}/>
-                            ))}
+                            <Col className='d-flex flex-column gap-2'>
+                                {products.map((product, index) => (
+                                    <CartProductCard product={product} selfIndexInCart={index} key={product.id}/>
+                                ))}
+                            </Col>
                             <h5 className='mt-3'>Amount: {totalProducts} Quantity: {totalQuantity}</h5>
                         </>
                     ) : (
@@ -26,17 +27,19 @@ const Cart = () => {
                     )}
                 </Col>
                 <Col xs={12} md={4}>
-                    <h3>Order</h3>
-                    <Form.Group className="mb-3 w-50" controlId="formBasicUsername">
-                        <Form.Label>Promocode</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter promocode"
-                            name="promocode"
-                        />
-                    </Form.Group>
-                    <p>Total: $<b style={{textDecoration: 'line-through'}}>{total}</b> ${discountedTotal}</p>
-                    <Button onClick={() => navigate(routes.order.path)}>Make order</Button>
+                    <Card body>
+                        <h3>Order</h3>
+                        <p className='d-flex justify-content-between'>
+                            <span>Cost: </span> <span>${total}</span>
+                        </p>
+                        <p className='d-flex justify-content-between'>
+                            <span>Discount: </span> <span>${total - discountedTotal}</span>
+                        </p>
+                        <p className='d-flex justify-content-between'>
+                            <span>Total: </span> <h5 className='d-inline'>${discountedTotal}</h5>
+                        </p>
+                        <Button as={Link} to={routes.order.path} className='w-100'>Make order</Button>
+                    </Card>
                 </Col>
             </Row>
         </Container>
