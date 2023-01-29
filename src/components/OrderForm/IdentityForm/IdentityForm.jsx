5fc226115e7e5aafc2d4bd48adc03a7fa7b945cc
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-01-30 00:47:10 +0300

    applied new added wraps

--- src/components/OrderForm/IdentityForm/IdentityForm.jsx
+++ src/components/OrderForm/IdentityForm/IdentityForm.jsx
@@ -1,46 +1,31 @@
-import React from 'react';
-import {Card, Col, Form} from "react-bootstrap";
+import {Card, Col} from "react-bootstrap";
+import FormTextField from "../../../utils/formik/FormTextField";
 
-const IdentityForm = ({values, touched, errors, handleChange, handleBlur}) => {
+const IdentityForm = () => {
     return (
         <Card body>
             <h5>Identity data</h5>
-            <Form.Group as={Col} className="mb-3" controlId="formBasicUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter username"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.username && !errors.username}
-                    isInvalid={touched.username && !!errors.username}
+            <div className={'mb-3'}>
+                <FormTextField
+                    as={Col}
+                    controlId="formBasicUsername"
+                    label='Username'
+                    placeholder='Enter your name'
+                    name='username'
+                    type='text'
                 />
-                <Form.Control.Feedback type="invalid">
-                    {touched.username && errors.username}
-                </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    required
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.email && !errors.email}
-                    isInvalid={touched.email && !!errors.email}
+            </div>
+            <div className={'mb-3'}>
+                <FormTextField
+                    as={Col}
+                    controlId="formBasicEmail"
+                    label='Email'
+                    placeholder='Enter email'
+                    name='email'
+                    type='email'
+                    formBottomText="We'll never share your confidential data"
                 />
-                <Form.Control.Feedback type="invalid">
-                    {touched.email && errors.email}
-                </Form.Control.Feedback>
-                <Form.Text className="text-muted">
-                    We'll never share your confidential data
-                </Form.Text>
-            </Form.Group>
+            </div>
         </Card>
     );
 };
