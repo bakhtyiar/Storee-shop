6fd29383b30d8d9582220bd73068f1dd7985047d
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-02-04 20:47:42 +0300

    added content at orderCompleted.jsx page

--- src/routes/orderCompleted.jsx
+++ src/routes/orderCompleted.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Container} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import {routes} from "../utils/constants";
 
 const OrderCompleted = () => {
+    const redirectAtS = 5;
+    const [remain, setRemain] = useState(redirectAtS);
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        setRemain((prevState) => prevState - 1);
+    }, 1000);
+
+    setTimeout(() => {
+        navigate(`${routes.orders.path}`);
+    }, redirectAtS * 1000)
+
     return (
-        <div>
-            <h1>Order completed</h1>
-        </div>
+        <Container className='mt-4'>
+            <h1 className=''>Order is successfully completed</h1>
+            <p>You will be redirected in {remain} seconds</p>
+        </Container>
     );
 };
 
