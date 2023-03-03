e21962b0eab839dc4a5c339654ffbf1a73c064ec
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-03-03 22:30:37 +0300

    added typeError for wrong function usage

--- src/utils/str/str.js
+++ src/utils/str/str.js
@@ -1,4 +1,7 @@
 export const capitalizeStr = (str) => {
+    if (typeof str !== 'string') {
+        throw new TypeError('Passed argument must be a string');
+    }
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
