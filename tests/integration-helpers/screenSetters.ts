6a79e4524a863113448795b146a5ad1c1c5dae53
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-13 20:16:36 +0300

    added unified method to set relevant screen size

--- tests/integration-helpers/screenSetters.ts
+++ tests/integration-helpers/screenSetters.ts
@@ -1,7 +1,21 @@
+import {TestIteration} from "../runs";
+
 export const setDesktopScreenSize = async () => {
+    // @ts-ignore
     await browser.setWindowSize(1260, 680);
 }
 
 export const setMobilePortraitScreenSize = async () => {
+    // @ts-ignore
     await browser.setWindowSize(360, 640);
+}
+
+export const setRelevantScreen = async (run: TestIteration): Promise<void> => {
+    if (run.options.deviceType === 'mobile' && run.options.orientation === 'portrait') {
+        await setMobilePortraitScreenSize();
+    } else if (run.options.deviceType === 'desktop' && run.options.orientation === 'album') {
+        await setDesktopScreenSize();
+    } else {
+        throw new Error('No relevant test type')
+    }
 }
