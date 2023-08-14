b80621e403bb47cc3626b1aafa7c9abdc0d94154
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-14 14:11:43 +0300

    installed theme to the body component

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,20 @@ function ErrorFallback({
     return (<div role="alert">
         \n
         <p>Something went wrong:</p>
-        
+
         <pre>{error.message}</pre>
-        
+
         <button onClick={resetErrorBoundary}>Try again</button>
     </div>)
 }
 
 function App() {
-    const {authUserState: {isLoggedIn, id, onLogin}, cartState: {onSetCart}} = useContext(RootContext);
+    const {authUserState: {isLoggedIn, id, onLogin}, cartState: {onSetCart}, themeState: {theme}} = useContext(RootContext);
+
+    useEffect( () => {
+        document.body.dataset.bsTheme = theme;
+    }, [theme])
+
     useEffect(() => {
         const authCookie = getCookie(userKey);
         if (authCookie !== undefined && authCookie !== '-1') {
@@ -51,31 +56,31 @@ function App() {
     }, []);
 
     return (<>
-        
+
         <BurgerMenuContextProvider>
-            
+
             <AuthModalContextProvider>
-                
+
                 <Header/>
             </AuthModalContextProvider>
         </BurgerMenuContextProvider>
-        
+
         <ErrorBoundary
             FallbackComponent={ErrorFallback}
             onReset={() => {
                 // reset the state of your app so the error doesn't happen again
             }}
         >
-            
+
             <div className='d-flex flex-grow-1 pt-4'>
-                
+
                 <Container>
-                    
+
                     <AppRoutes/>
                 </Container>
             </div>
         </ErrorBoundary>
-        
+
         <Footer/>
     </>);
 }
