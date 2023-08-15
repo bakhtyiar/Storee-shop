6bc51f594f76b4a758d5329a07792b9fa301eb46
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-08-15 17:44:34 +0300

    updated for relevant using capitalizing str method

--- src/utils/str/str.ts
+++ src/utils/str/str.ts
@@ -1,5 +1,5 @@
 export const capitalizeStr = (str: string): string => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 export const replaceDashToSpace = (str: string): string => {
