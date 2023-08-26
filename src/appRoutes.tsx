916dd8f917431bf85313de3e363861eb0e38fc2a
Bakhtiyar <bakhtiyarkhuramshin@gmail.com>
2023-08-26 13:34:22 +0300

    Made lazy imports and suspense on appRoutes.tsx

--- src/appRoutes.tsx
+++ src/appRoutes.tsx
@@ -1,41 +1,47 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import {Navigate, Route, Routes} from "react-router-dom";
 import {routes} from "./utils/constants";
-import Home from "./routes/home";
-import Products from "./routes/products";
-import Product from "./routes/product";
-import News from "./routes/news";
-import Profile from "./routes/profile";
-import NotFound from "./routes/notFound";
 import * as PropTypes from "prop-types";
-import Register from "./routes/register";
-import Login from "./routes/login";
-import Cart from "./routes/cart";
-import OrderMaking from "./routes/orderMaking";
-import OrderCompleted from "./routes/orderCompleted";
-import NewsPost from "./components/NewsPost/NewsPost";
 import {AuthModalContextProvider} from "./contexts/authModal-context/authModal-context";
+import LoaderIndicatorCentral from "./components/LoaderIndicator/LoaderIndicatorCental";
+
+const Home = lazy(() => import("./routes/home"))
+const Products = lazy(() => import("./routes/products"))
+const Product = lazy(() => import("./routes/product"))
+const News = lazy(() => import("./routes/news"))
+const Profile = lazy(() => import("./routes/profile"))
+const NotFound = lazy(() => import("./routes/notFound"))
+const Register = lazy(() => import("./routes/register"))
+const Login = lazy(() => import("./routes/login"))
+const Cart = lazy(() => import("./routes/cart"))
+const OrderMaking = lazy(() => import("./routes/orderMaking"))
+const OrderCompleted = lazy(() => import("./routes/orderCompleted"))
+const NewsPost = lazy(() => import("./components/NewsPost/NewsPost"))
 
 function AppRoutes() {
     return (
-        <Routes>
-            <Route path={routes.home.path} element={<Home/>}/>
-            <Route path={routes.products.path} element={<Navigate to={routes.products.path + "/1"} replace={true}/>}/>
-            <Route path={routes.products.path + "/:page"} element={<Products/>}/>
-            <Route path={routes.product.path} element={<Navigate to={routes.products.path}/>}/>
-            <Route path={routes.product.path + "/:id"} element={<Product/>}/>
-            <Route path={routes.news.path} element={<News/>}/>
-            <Route path={routes.newsPost.path} element={<Navigate to={routes.news.path}/>}/>
-            <Route path={routes.newsPost.path + "/:id"} element={<NewsPost/>}/>
-            <Route path={routes.register.path} element={<Register/>}/>
-            <Route path={routes.login.path} element={<Login/>}/>
-            <Route path={routes.profile.path} element={<AuthModalContextProvider><Profile/></AuthModalContextProvider>}/>
-            <Route path={routes.cart.path} element={<Cart/>}/>
-            <Route path={routes.orderMaking.path} element={<OrderMaking/>}/>
-            <Route path={routes.orderCompleted.path} element={<OrderCompleted/>}/>
-            <Route path="*" element={<Navigate to={routes.notFound.path}/>}/>
-            <Route path={routes.notFound.path} element={<NotFound/>}/>
-        </Routes>
+        <Suspense fallback={<LoaderIndicatorCentral/>}>
+            <Routes>
+                <Route path={routes.home.path} element={<Home/>}/>
+                <Route path={routes.products.path}
+                       element={<Navigate to={routes.products.path + "/1"} replace={true}/>}/>
+                <Route path={routes.products.path + "/:page"} element={<Products/>}/>
+                <Route path={routes.product.path} element={<Navigate to={routes.products.path}/>}/>
+                <Route path={routes.product.path + "/:id"} element={<Product/>}/>
+                <Route path={routes.news.path} element={<News/>}/>
+                <Route path={routes.newsPost.path} element={<Navigate to={routes.news.path}/>}/>
+                <Route path={routes.newsPost.path + "/:id"} element={<NewsPost/>}/>
+                <Route path={routes.register.path} element={<Register/>}/>
+                <Route path={routes.login.path} element={<Login/>}/>
+                <Route path={routes.profile.path}
+                       element={<AuthModalContextProvider><Profile/></AuthModalContextProvider>}/>
+                <Route path={routes.cart.path} element={<Cart/>}/>
+                <Route path={routes.orderMaking.path} element={<OrderMaking/>}/>
+                <Route path={routes.orderCompleted.path} element={<OrderCompleted/>}/>
+                <Route path="*" element={<Navigate to={routes.notFound.path}/>}/>
+                <Route path={routes.notFound.path} element={<NotFound/>}/>
+            </Routes>
+        </Suspense>
     );
 }
 
